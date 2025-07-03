73b9e1bc2c2410ba89dba05d9484b787947a0bd6
lavi324 <lavialduby@gmail.com>
2025-07-03 16:18:46 +0000

    Initial commit

--- push.sh
+++ push.sh
@@ -3,7 +3,7 @@
 USERNAME="lavi324"
 TOKEN="${GITHUB_PAT}"
 
-git remote set-url origin https://$USERNAME:$TOKEN@github.com/$USERNAME/sport-tables-ai.git
+git remote set-url origin https://$USERNAME:$TOKEN@github.com/$USERNAME/https-setup.git
 
 git add .
 git commit -m "Initial commit"
